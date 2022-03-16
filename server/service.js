2465afbb000e84dc094bf5abd041c9b5e1cba9ce
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2022-03-16 14:19:26 -0300

    create get and remove cliente stream

--- server/service.js
+++ server/service.js
@@ -2,12 +2,33 @@ import fs from 'fs'
 import fsPromises from 'fs/promises'
 import config from './config.js'
 import { join, extname } from 'path'
+import { randomUUID } from 'crypto'
+import { PassThrough } from 'stream'
 
 const {
     dir: { __public },
 } = config
 
 export class Service {
+    constructor() {
+        this.clientStreams = new Map()
+    }
+
+    getClientStream() {
+        const id = randomUUID()
+        const clientStream = new PassThrough()
+        this.clientStreams.set(id, clientStream)
+
+        return {
+            id,
+            clientStream,
+        }
+    }
+
+    removeClienteStream(clientID) {
+        this.clientStreams.delete(clientID)
+    }
+
     createFileStream(filename) {
         return fs.createReadStream(filename)
     }
